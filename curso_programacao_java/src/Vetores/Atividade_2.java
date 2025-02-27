2e264a4ed3f37d4a47e74c8e0ba1a4af5289d2c9
Kauha-Costa <kauhaerickcosta@gmail.com>
2025-02-27 16:54:40 +0000

    Atividade vetores_01

---
+++ curso_programacao_java/src/Vetores/Atividade_2.java
@@ -0,0 +1,13 @@
+package Vetores;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Atividade_2 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        
+
+    }
+}
